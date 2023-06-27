b7d1a06eb126a15ed0092f5963b425c68af36bde
kiruthikasoundar <36938852+kiruthikasoundar@users.noreply.github.com>
2023-06-27 11:23:03 -0400

    using env value

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
                       color='inherit'
                       size="large"
                       variant='outlined'
-                      startIcon={<GoogleIcon />}  onClick={() => {window.location.replace('http://localhost:8080/signin')}}     
+                      startIcon={<GoogleIcon />}  onClick={() => {window.location.replace('${import.meta.env.VITE_API_URI}/signin')}}     
                 >
                 Login
                 </Button>  
